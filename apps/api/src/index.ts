4dd7c52f481de9edc68dd69175c60279c3bfc8d3
Petr Bui <buihonganh1@gmail.com>
2024-08-06 07:30:02 +0200

    :package: chore: add JEST & SWAGGER

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,12 +4,14 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 const app = express()
-
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from '../swagger.json'
 app.use(morgan('dev'))
 
 app.use(urlencoded({ extended: true }))
 app.use(json())
 app.use(cors())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = process.env.PORT || 5001
 
